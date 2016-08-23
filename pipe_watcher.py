f123b3b20e2299a937a8a6d42eeabdd733507309
David Diepenbrock <ddiepo@gmail.com>
2016-08-23 03:50:28 +0000

    Restructured logic to move more into the class. Added starting ffmpeg for the capture stream. Untested logic added to mark files in the capture stream on motion start and stop.

--- pipe_watcher.py
+++ pipe_watcher.py
@@ -1,4 +1,4 @@
-import os
+import os, os_help       
 
 class PipesWatcher:
     """ This class watches pipes and when there is data reads a line at a time and sends to the 
@@ -7,8 +7,7 @@ class PipesWatcher:
     def __init__(self, files_to_monitor):
         self._pipes = []
         for path in files_to_monitor:
-            if (os.path.exists(path)):
-                os.unlink(path)
+            os_help.ignore_exist(os.unlink, path)
             os.mkfifo(path)
             pipe_fd = os.open(path, os.O_RDONLY | os.O_NONBLOCK)
             self._pipes.append((os.fdopen(pipe_fd), path))
@@ -16,10 +15,7 @@ class PipesWatcher:
     def __del__(self):
         for pipe in self._pipes:
             pipe[0].close()
-            try:
-                os.unlink(pipe[1])
-            except OSError:
-                pass
+            os_help.ignore_exist(os.unlink, pipe[1])
             
     def check(self, func):
         for pipe in self._pipes:
--- surveillance.py
+++ surveillance.py
@@ -1,4 +1,11 @@
-import os, pipe_watcher, time, subprocess
+import os
+import re
+import subprocess
+import shutil
+import time
+
+import os_help
+import pipe_watcher
 
 ramdisk="/dev/shm/"
 
@@ -8,68 +15,163 @@ motion_fps = 1
 
 # Global variable to store all our camera stuff
 camera_item = []
+video_path=ramdisk + "/video/"
+motion_is_active = False
 
 class CameraItems:
     """ Data structure for storing information about a single camera """
-    def __init__(self, camera_index, pipe):
+    def __init__(self, camera_index):
+        print "running init"
+        self.capture_prefix = "cam" + str(camera_index)
         self.capture_url = cameras[camera_index][0]
+        self.capture_path = video_path + str(camera_index)
+        self.capture_process = None
+        self.save_prefix = "save-"
         self.monitor_url = cameras[camera_index][1]
         self.monitor_jpg = ramdisk + "cam" + str(camera_index) + ".jpg"
-        self.pipe = pipe
-        self.is_active = False
+        self.pipe = ramdisk + "cam_motion_" + str(camera_index)
         self.motion_process = None
-
-def on_change(message, pipe):
-    if (message == "on"):
-        print ("It's On: " + pipe)
-    else:
-        print ("It's Off: " + pipe)
+        self.motion_is_active = False
+        self.motion_start_num = None
         
-def start_ffmpeg_monitor():
-    FNULL = open(os.devnull, 'w')
-    for camera in camera_item:
+        os_help.ignore_exist(os.mkdir, self.capture_path)
+        self.start_monitor()
+        self.start_capture()
+        
+    def cleanup(self):
+        # Stop ffmpeg monitor
+        try:
+            self.motion_process.terminate()
+        except:
+            pass
+        
+        try:
+            self.capture_process.terminate()
+        except:
+            pass
+        
+        # Clean up the jpg's from ffmpeg
+        os_help.ignore_exist(os.unlink, self.monitor_jpg)
+        
+        # Clean up captures, ignore errors
+        shutil.rmtree(self.capture_path, True);
+    
+    def start_monitor(self):
+        FNULL = open(os.devnull, 'w')
         _cmd = ["ffmpeg", 
                 "-y", 
-                "-i", camera.monitor_url,
+                "-i", self.monitor_url,
                 "-r", str(motion_fps),
                 "-updatefirst", "1",
-                camera.monitor_jpg]
-        proc = subprocess.Popen(_cmd, stdout=FNULL, stderr=subprocess.STDOUT)
-        camera.motion_process = proc
-        print "ffmpeg monitoring ", camera.monitor_url, " pid: ", proc.pid
+                self.monitor_jpg]
+        self.motion_process = subprocess.Popen(_cmd, stdout=FNULL, stderr=subprocess.STDOUT)
+        print "ffmpeg monitoring ", self.monitor_url, " pid: ", self.motion_process.pid
+        
+    def start_capture(self):
+        FNULL = open(os.devnull, 'w')
+        _cmd = ["ffmpeg", 
+                "-i", self.capture_url,
+                "-c", "copy",
+                "-f", "segment",
+                "-segment_time", "300",
+                "-segment_format", "mp4",
+                self.capture_path + "/" + self.capture_prefix + "-%05d.mp4"]
+        self.capture_process = subprocess.Popen(_cmd, stdout=FNULL, stderr=subprocess.STDOUT)
+        print "ffmpeg capturing ", self.capture_url, " pid: ", self.capture_process.pid
+        
+    def get_latest_capture_number(self):
+        _files = os.listdir(self.capture_path)
+        _largest = NONE
+        for _file in _files:
+            if (os.path.isfile(_file)):
+                _s = re.split('\.|_', _file)
+                if (_s[0] == self.capture_prefix):
+                    try:
+                        _val = int(_s[1])
+                        if (_val > _largest):
+                            _largest = val
+                    except:
+                        pass
+        return _largest
+    
+    def mark_capture_start(self):
+        latest = self.get_latest_capture_number()
+        if (latest == NONE):
+            return
+        open(self.save_prefix + str(latest), 'a').close()
+        if (latest > 1):
+            open(self.save_prefix + str(latest - 1), 'a').close()
+            
+    def get_capture_start_number(self):
+        _smallest = NONE
+        for _file in os.listdir(self.capture_path):
+            _s = _file.split("-")
+            if (_s[0] == self.save_prefix):
+                try:
+                    _val = int(_s[1])
+                    if _val < _smallest:
+                        _smallest = _val
+                except:
+                    pass
+        return _smallest
+        
+    def mark_capture_stop(self):
+        latest = self.get_latest_capture_number()
+        start = get_capture_start_number()
+        if (latest == NONE or start == NONE):
+            return
+        for x in range(start + 1, latest + 1):
+            open(self.save_prefix + str(x), 'a').close()
+
+def on_change(message, pipe):
+    is_on = (message == "on")
+    state_change = True;
+    for camera in camera_item:
+        if camera.pipe == pipe:
+            camera.motion_is_active = is_on;
+        elif camera.motion_is_active:
+            state_change = False;
+            break;
+    
+    if state_change:
+        for camera in camera_item:
+            if is_on:
+                camera.mark_capture_start()
+            else:
+                camera.mark_capture_stop()
+        
+def save_marked_files():
+    print "Saving marked files"
+
+def initialize_cameras():
+    os_help.ignore_exist(os.mkdir, video_path)
+    for index, camera in enumerate(cameras):
+        _cam = CameraItems(index)
+        camera_item.append(_cam)
 
 def get_pipes():
     _pipes = []
-    for index, camera in enumerate(cameras):
-        _pipe = ramdisk + "cam_motion_" + str(index)
-        _pipes.append(_pipe)
-        _cam = CameraItems(index, _pipe)
-        camera_item.append(_cam)
+    for camera in camera_item:
+        _pipes.append(camera.pipe)
     return _pipes
 
 try:
-    pipes = get_pipes()
-    my_input = pipe_watcher.PipesWatcher(pipes)
+    initialize_cameras()
+    my_input = pipe_watcher.PipesWatcher(get_pipes())
 
-    print pipes
-    start_ffmpeg_monitor()
-
-
+    start_time = time.time()
     while True:
         my_input.check(on_change)
-        time.sleep(1)
+        now = time.time()
+        if (now - start_time > 120):
+            start_time = now
+            save_marked_files()
+        else:
+            time.sleep(1)
 
 finally:
     for camera in camera_item:
-        # Stop ffmpeg monitor
-        try:
-            print "Cleaning up camera: ", camera.monitor_url, " pid: ", camera.motion_process.pid
-            camera.motion_process.terminate()
-        except NameError:
-            print "No process for: ", camera_item.monitor_url
+        camera.cleanup()
         
-        try:
-            # Clean up the jpg's from ffmpeg
-            os.unlink(camera.monitor_jpg)
-        except OSError:
-            pass
+    shutil.rmtree(video_path, True);
+    
