e5a0eea1790e65f17a67f9655bfc08ed00ab34ba
ddiepo <ddiepo@gmail.com>
2018-02-25 21:16:33 -0600

    Getting all the date matching stuff to work.

--- os_help.py
+++ os_help.py
@@ -8,8 +8,15 @@ def ignore_exist(func, path):
     except OSError as exc:  # Python >2.5
         if exc.errno == errno.EEXIST and os.path.isdir(path):
             pass
-        elif exc.errno == errno.ENOENT:
+        else:
+            raise
+        
+def ignore_exist(func, src, dest):
+    """ Ignore errno.EEXIST or errno.ENOENT exceptions """
+    try:
+        func(src, dest)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == errno.EEXIST:
             pass
         else:
-            print "Error: ", exc.errno
             raise
--- surveillance.py
+++ surveillance.py
@@ -2,7 +2,9 @@ import os
 import re
 import subprocess
 import shutil
+import datetime
 import time
+import traceback
 
 import os_help
 import pipe_watcher
@@ -38,22 +40,27 @@ cameras = [ ("back",
 camera_item = []
 working_area=ramdisk + "surveillance/"
 motion_config_path=working_area + "motion_config/"
-unprocessed_videos=video_store_path + "/unprocessed"
+video_unprocessed_dir=video_store_path + "/unprocessed"
+video_motion_dir="/motion"
+video_all_dir="/all"
+segment_length = 300 # seconds
+event_gap = 15  # time before or after motion
+motion_pid = None
+date_time_format = "%Y%m%d-%H%M%S%Z"
 
 class CameraItems:
     """ Data structure for storing information about a single camera """
     def __init__(self, camera_index):
         print "running init"
         self.camera_name = cameras[camera_index][0]
-        self.capture_prefix = "cam" + str(camera_index)
         self.capture_url = cameras[camera_index][1]
-        self.capture_path = unprocessed_videos + '/' + self.camera_name
+        self.capture_path = video_unprocessed_dir + '/' + self.camera_name
         self.capture_process = None
         self.save_prefix = "save-"
+        self.motion_index = camera_index + 1
         self.monitor_url = cameras[camera_index][2]
-        self.pipe = working_area + "motion_pipe_cam" + str(camera_index + 1)
-        self.motion_is_active = False
-        self.motion_start_num = None
+        self.pipe = working_area + "motion_pipe_cam" + str(self.motion_index)
+        self.motion_start_time = None
         
         os_help.ignore_exist(os.makedirs, self.capture_path)
         self.write_motion_config()
@@ -68,8 +75,8 @@ class CameraItems:
         # Clean up the camera config (probably don't need this because the whole working area will be blown away
         os_help.ignore_exist(os.unlink, motion_config_path + self.camera_name + '.cfg')
         
-        # TODO link the videos for motion or not first!
-        shutil.rmtree(self.capture_path, True);
+        self.process_segments()
+        shutil.rmtree(self.capture_path, True)
     
     def write_motion_config(self):
         thread_conf_file = open(motion_config_path + self.camera_name + '.cfg', 'w')
@@ -77,70 +84,86 @@ class CameraItems:
         maskFile = motion_mask_path + '/' + self.camera_name + '.pgm'
         if os.path.isfile(maskFile):
             thread_conf_file.write("mask_file " + maskFile + '\n')
-        
+            
         motion_conf_file = open(motion_config_path + 'motion.cfg', 'a')
         motion_conf_file.write("thread " + thread_conf_file.name + '\n')
         print "created motion config file: " + thread_conf_file.name
-        
+
     def start_capture(self):
         FNULL = open(os.devnull, 'w')
         _cmd = ["ffmpeg", 
+                "-rtsp_transport", "tcp",
+                "-stimeout", "500000",  # TCP Timeout for the stream
                 "-i", self.capture_url,
                 "-c", "copy",
                 "-f", "segment",
-                "-segment_time", "300",
+                "-segment_time", str(segment_length),
                 "-segment_format", "mp4",
-                self.capture_path + "/" + self.capture_prefix + "-%05d.mp4"]
+                "-reset_timestamps", "1",
+                "-strftime", "1",
+                self.capture_path + "/" + self.camera_name + "." + date_time_format + ".mp4"]
         self.capture_process = subprocess.Popen(_cmd, stdout=FNULL, stderr=subprocess.STDOUT)
         print "ffmpeg capturing ", self.capture_url, " pid: ", self.capture_process.pid
         
-    def get_latest_capture_number(self):
-        # TODO this function needs to be revisited!
-        _files = os.listdir(self.capture_path)
-        _largest = NONE
-        for _file in _files:
-            if (os.path.isfile(_file)):
-                _s = re.split('\.|_', _file)
-                if (_s[0] == self.capture_prefix):
-                    try:
-                        _val = int(_s[1])
-                        if (_val > _largest):
-                            _largest = val
-                    except:
-                        pass
-        return _largest
     
     def mark_capture_start(self):
-        # TODO this function needs to be revisited!
-        latest = self.get_latest_capture_number()
-        if (latest == NONE):
-            return
-        open(self.save_prefix + str(latest), 'a').close()
-        if (latest > 1):
-            open(self.save_prefix + str(latest - 1), 'a').close()
+        self.motion_start_time = datetime.datetime.now()
+        
+    def process_segments(self):
+        now = datetime.datetime.now()
+        print "Processing segments for: " + self.camera_name
+        file_list = os.listdir(self.capture_path)
+        newest_file = None
+        newest_start_time = None
+        for _file in file_list:
+            pieces = _file.split('.')
+            if len(pieces) < 2 or pieces[len(pieces)-1] != "mp4":
+                print "Unexpected pieces count: " + str(pieces)
+                continue
+            try:
+                segment_start_time = datetime.datetime.strptime(pieces[len(pieces)-2], date_time_format)
+                if newest_start_time == None or segment_start_time > newest_start_time:
+                    newest_start_time = segment_start_time
+                    newest_file = _file
+                date_stamp = segment_start_time.strftime('%Y.%m.%d')
+                motion_file_destination = video_store_path + "/" + date_stamp + "/" + video_motion_dir + "/" + _file
+                if self.motion_start_time != None \
+                   and (segment_start_time - datetime.timedelta(seconds=event_gap)) > self.motion_start_time \
+                   and (segment_start_time + datetime.timedelta(seconds=segment_length)) < now \
+                   and not os.path.exists(motion_file_destination):
+                    
+                    os_help.ignore_exist(os.makedirs, os.path.dirname(motion_file_destination))
+                    os_help.ignore_exist(os.link(self.capture_path + "/" + _file, motion_file_destination))
+            except:
+                traceback.print_exc()
             
-    def get_capture_start_number(self):
-        # TODO this function needs to be revisited!
-        _smallest = NONE
-        for _file in os.listdir(self.capture_path):
-            _s = _file.split("-")
-            if (_s[0] == self.save_prefix):
-                try:
-                    _val = int(_s[1])
-                    if _val < _smallest:
-                        _smallest = _val
-                except:
-                    pass
-        return _smallest
+        if newest_file == None:
+            print "No newest file?  Odd, but okay... bailing out"
+            return
+        
+        for _file in file_list:
+            try:
+                pieces = _file.split('.')
+                segment_start_time = datetime.datetime.strptime(pieces[len(pieces)-2], date_time_format)
+                date_stamp = segment_start_time.strftime('%Y.%m.%d')
+                destination = video_store_path + "/" + date_stamp + "/" + video_all_dir + "/" + _file
+                os_help.ignore_exist(os.makedirs, os.path.dirname(destination))
+                os_help.ignore_exist(os.link(self.capture_path + "/" + _file, destination))
+                
+                if (_file != newest_file):
+                    os.unlink(self.capture_path + "/" + _file)
+                else:
+                    print "Keeping the file currently being written to: " + _file
+            except:
+                traceback.print_exc()
+                print "Exception happened"
+            
+    def restart_process_if_died(self):
+        print "TODO verify capture_pid is still running" # TODO
         
     def mark_capture_stop(self):
-        # TODO this function needs to be revisited!
-        latest = self.get_latest_capture_number()
-        start = get_capture_start_number()
-        if (latest == NONE or start == NONE):
-            return
-        for x in range(start + 1, latest + 1):
-            open(self.save_prefix + str(x), 'a').close()
+        process_segments()
+        this.motion_start_time = None
 
 def write_base_motion_config_file():
     os_help.ignore_exist(os.makedirs, motion_config_path)
@@ -150,30 +173,19 @@ def write_base_motion_config_file():
     motion_conf_file.write("output_pictures off\n")
     motion_conf_file.write("on_event_start /home/pi/flagStart.bash %t\n")
     motion_conf_file.write("on_event_end /home/pi/flagEnd.bash %t\n")
-    motion_conf_file.write("event_gap 15\n")
+    motion_conf_file.write("event_gap " + str(event_gap) + "\n")
     motion_conf_file.write("log_level 4\n")
     motion_conf_file.write("\n")
 
 def on_change(message, pipe):
     is_on = (message == "on")
-    state_change = True;
     for camera in camera_item:
-        if camera.pipe == pipe:
-            camera.motion_is_active = is_on
+        if camera.pipe == pipe and (camera.motion_start_time != None) != is_on:
             print "pipe called for camera: " + camera.camera_name + " msg: " + message
-        elif camera.motion_is_active:
-            state_change = False
-            break
-    
-#    if state_change:
-#        for camera in camera_item:
-#            if is_on:
-#                camera.mark_capture_start()
-#            else:
-#                camera.mark_capture_stop()
-        
-def save_marked_files():
-    print "Saving marked files"
+            if is_on:
+                camera.mark_capture_start()
+            else:
+                camera.mark_capture_stop()
 
 def initialize_cameras():
     write_base_motion_config_file()
@@ -187,25 +199,37 @@ def get_pipes():
         _pipes.append(camera.pipe)
     return _pipes
 
+def start_motion_detection():
+    FNULL = open(os.devnull, 'w')
+    _cmd = ["motion", 
+            "-c", motion_config_path + "/motion.cfg"]
+#    motion_pid = subprocess.Popen(_cmd, stdout=FNULL, stderr=subprocess.STDOUT)
+
 try:
     shutil.rmtree(working_area, True);
     os_help.ignore_exist(os.makedirs, working_area)
     initialize_cameras()
     my_input = pipe_watcher.PipesWatcher(get_pipes())
+    start_motion_detection()
 
-    start_time = time.time()
+    start_time = datetime.datetime.now()
     while True:
         my_input.check(on_change)
-        now = time.time()
-        if (now - start_time > 120):
+        now = datetime.datetime.now()
+        if (now - start_time) > datetime.timedelta(seconds=segment_length):
             start_time = now
-            save_marked_files()
+            
+            for camera in camera_item:
+                camera.process_segments()
+                camera.restart_process_if_died()
+            
         else:
             time.sleep(1)
 
 finally:
+    print "Shutting down..."
     for camera in camera_item:
         camera.cleanup()
     
-    shutil.rmtree(unprocessed_videos, True);
+    shutil.rmtree(video_unprocessed_dir, True);
     
